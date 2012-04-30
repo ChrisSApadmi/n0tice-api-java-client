4819910cfdc2480d8b24128965304a06d1f72634
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-04-30 19:06:36 +0100

    Equals and hash code so that we can do contains checks on lists of users.

--- src/main/java/com/n0tice/api/client/model/User.java
+++ src/main/java/com/n0tice/api/client/model/User.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 public class User {
 	
-	private final String username;
+	private String username;
 	private String displayName;
 	private String bio;
 	private Image profileImage;
-	private final List<String> noticeboards;
-	private final List<String> followedNoticeboards;
-	private final List<User> followedUsers;
+	private List<String> noticeboards;
+	private List<String> followedNoticeboards;
+	private List<User> followedUsers;
 	
+	public User(String username) {
+		this.username = username;
+	}
+
 	public User(String username, String displayName, String bio, Image profileImage, List<String> noticeboards, List<String> followedNoticeboards, List<User> followedUsers) {
 		this.username = username;
 		this.displayName = displayName;
@@ -48,6 +52,32 @@ public class User {
 	
 	public List<User> getFollowedUsers() {
 		return followedUsers;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
 	}
 
 	@Override
