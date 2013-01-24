e93d315908aedab0ce5dc787ca03bcf7ad4711b7
ChrisSApadmi <chriss@apadmi.com>
2013-01-24 12:26:25 +0000

    Added image parsing for MediumDouble MediumLandscape MediumLandscapeDouble

--- src/main/java/com/n0tice/api/client/model/Image.java
+++ src/main/java/com/n0tice/api/client/model/Image.java
@@ -1,39 +1,66 @@
 package com.n0tice.api.client.model;
 
-public class Image {
-	
+public class Image
+{
+
 	private final String small;
 	private final String medium;
+	private final String mediumDouble;
 	private final String large;
 	private final String orientation;
-	
-	public Image(String small, String medium, String large, String orientation) {
+	private final String mediumLandscape;
+	private final String mediumLandscapeDouble;
+
+	public Image(String small, String medium, String mediumDouble, String mediumLandscape, String mediumLandscapeDouble, String large, String orientation)
+	{
 		this.small = small;
 		this.medium = medium;
+		this.mediumDouble = mediumDouble;
+		this.mediumLandscape = mediumLandscape;
+		this.mediumLandscapeDouble = mediumLandscapeDouble;
 		this.large = large;
 		this.orientation = orientation;
 	}
 
-	public String getSmall() {
+	public String getSmall()
+	{
 		return small;
 	}
 
-	public String getMedium() {
+	public String getMedium()
+	{
 		return medium;
 	}
 
-	public String getLarge() {
+	public String getMediumDouble()
+	{
+		return mediumDouble;
+	}
+
+	public String getMediumLandscape()
+	{
+		return mediumLandscape;
+	}
+
+	public String getMediumLandscapeDouble()
+	{
+		return mediumLandscapeDouble;
+	}
+
+	public String getLarge()
+	{
 		return large;
 	}
-	
-	public String getOrientation() {
+
+	public String getOrientation()
+	{
 		return orientation;
 	}
 
 	@Override
-	public String toString() {
-		return "Image [large=" + large + ", medium=" + medium
-				+ ", orientation=" + orientation + ", small=" + small + "]";
+	public String toString()
+	{
+		return "Image [large=" + large + ", medium=" + medium + ",mediumDouble=" + mediumDouble + ", orientation=" + orientation + ", small=" + small + "]";
 	}
-		
+
 }
--- src/main/java/com/n0tice/api/client/parsers/ImageParser.java
+++ src/main/java/com/n0tice/api/client/parsers/ImageParser.java
@@ -5,15 +5,20 @@ import org.json.JSONObject;
 
 import com.n0tice.api.client.model.Image;
 
-public class ImageParser {
-	
+public class ImageParser
+{
+
 	private static final String SMALL = "small";
 	private static final String MEDIUM = "medium";
+	private static final String MEDIUMDOUBLE = "mediumdouble";
+	private static final String MEDIUMLANDSCAPE = "mediumlandscape";
+	private static final String MEDIUMLANDSCAPEDOUBLE = "mediumlandscapedouble";
 	private static final String LARGE = "large";
 	private static final String ORIENTATION = "orientation";
-	
-	public Image parseImage(JSONObject imageJson) throws JSONException {
-		return new Image(imageJson.getString(SMALL), imageJson.getString(MEDIUM), imageJson.getString(LARGE), 
+
+	public Image parseImage(JSONObject imageJson) throws JSONException
+	{
+		return new Image(imageJson.getString(SMALL), imageJson.getString(MEDIUM), imageJson.optString(MEDIUMDOUBLE), imageJson.optString(MEDIUMLANDSCAPE), imageJson.optString(MEDIUMLANDSCAPEDOUBLE), imageJson.getString(LARGE),
 				imageJson.has(ORIENTATION) ? imageJson.getString(ORIENTATION) : null);
 	}
 
